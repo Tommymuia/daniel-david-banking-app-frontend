b76387d71218c263d75b0bf7113dabd36e7f6652
danielkamweru <daniel.kamweru@student.moringaschool.com>
2026-01-15 11:40:21 +0300

    Refactor application routing configuration

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,30 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Contact from '../pages/Contact'
 import Login from '../pages/Login'
 import Signup from '../pages/Signup'
+import Dashboard from '../pages/Dashboard'
+import Accounts from '../pages/Accounts'
+import Transfer from '../pages/Transfer'
+import Transactions from '../pages/Transactions'
+import Settings from '../pages/Settings'
+import ProtectedRoute from '../components/ProtectedRoute'
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/accounts" element={<ProtectedRoute><Accounts /></ProtectedRoute>} />
+        <Route path="/transfer" element={<ProtectedRoute><Transfer /></ProtectedRoute>} />
+        <Route path="/transactions" element={<ProtectedRoute><Transactions /></ProtectedRoute>} />
+        <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
       </Routes>
     </Router>
   )
